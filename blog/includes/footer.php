69422447926567969946abe0571a8184f6b5b227
Mariusz Skoneczko <mariuszskon@users.noreply.github.com>
2015-08-19 16:37:37 +1000

    Made the social buttons appear a bit nicer

--- blog/includes/footer.php
+++ blog/includes/footer.php
@@ -1,5 +1,5 @@
                 </article>
-                <div class="social-buttons">
+                <div>
                     <div id="fb-root"></div>
                     <script>(function(d, s, id) {
                         var js, fjs = d.getElementsByTagName(s)[0];
@@ -8,10 +8,12 @@
                         js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.4";
                         fjs.parentNode.insertBefore(js, fjs);
                      }(document, 'script', 'facebook-jssdk'));</script>
-                    <div class="fb-share-button" data-layout="button"></div>
-                    <a href="https://twitter.com/share" class="twitter-share-button" data-via="mariuszskon" data-count="none">Tweet</a>
-                    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
-                    <a href="//www.reddit.com/submit" onclick="window.location = '//www.reddit.com/submit?url=' + encodeURIComponent(window.location); return false"> <img src="//www.redditstatic.com/spreddit7.gif" alt="submit to reddit" border="0" /> </a>
+                    <ul class="social-buttons">
+                        <li><div class="fb-share-button" data-layout="button"></div></li>
+                        <li><a href="https://twitter.com/share" class="twitter-share-button" data-via="mariuszskon" data-count="none">Tweet</a></li>
+                        <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+                        <li><a href="//www.reddit.com/submit" onclick="window.location = '//www.reddit.com/submit?url=' + encodeURIComponent(window.location); return false"> <img src="//www.redditstatic.com/spreddit7.gif" alt="submit to reddit" border="0" /> </a></li>
+                    </ul>
                 </div>
             </div>
         </div>
