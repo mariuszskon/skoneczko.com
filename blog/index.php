e21ae50c7fa555372377665d06f096467be1b6ee
Mariusz Skoneczko <mariuszskon@users.noreply.github.com>
2015-08-31 19:50:43 +1000

    Added a blog post about Git

--- blog/index.php
+++ blog/index.php
@@ -46,6 +46,8 @@
                     <p class="warn">You are using Internet Explorer lower than 9.<br />Please <a href="http://browsehappy.com/">upgrade your browser</a> to make the most of the web.</p>
                 <![endif]-->
                 <h2>Latest posts</h2>
+                <h3><a href="2015/08/31/gitstarted.php">Git Started</a></h3>
+                <div id="date-posted">Posted on 31st August 2015</div><br>
                 <h3><a href="2015/08/19/sass.php">Sass is awesome</a></h3>
                 <div class="date-posted">Posted on 19th August 2015</div><br>
                 <h3><a href="2015/08/15/browsersupport.php">Web browser support</a></h3>
