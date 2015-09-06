f0a314ed939da7ff126f08d700e13c6bcb927211
Mariusz Skoneczko <mariuszskon@users.noreply.github.com>
2015-09-06 14:12:44 +1000

    Initialized npm repo and removed no longer valid build.sh

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# a small simple build script before I switch to a better build system
-
-sass -t expanded src/base.scss css/style.css
-sass -t compressed --sourcemap=none src/base.scss css/style.min.css
