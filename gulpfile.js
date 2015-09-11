f544f558798749009d1817bbd3cf9908f768a8a5
Mariusz Skoneczko <mariuszskon@users.noreply.github.com>
2015-09-11 19:40:32 +1000

    Copy html and php files from src to dist (only if changed)

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,12 @@ var sass = require('gulp-sass');
 var rename = require('gulp-rename');
 var minifycss = require('gulp-minify-css');
 var autoprefixer = require('gulp-autoprefixer');
+var debug = require('gulp-debug');
+var changed = require('gulp-changed');
 
 gulp.task('sass', function() {
     gulp.src('src/src/base.scss')
+        .pipe(debug({title: 'gulp-debug [sass]:'}))
         .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
         .pipe(autoprefixer({browsers: ['last 3 version']}))
         .pipe(rename('style.css'))
@@ -13,4 +16,19 @@ gulp.task('sass', function() {
         .pipe(rename({suffix: '.min'}))
         .pipe(minifycss())
         .pipe(gulp.dest('dist/css'))
+});
+
+gulp.task('html', function() {
+    gulp.src(['src/**/*.html', '!**/*~']) // don't copy backup files
+        .pipe(changed('dist/'))
+        .pipe(debug({title: 'gulp-debug [html]'}))
+        .pipe(gulp.dest('dist/'));
+});
+
+// blog task separate, in case changes are to be made later
+gulp.task('blog', function() {
+    gulp.src(['src/blog/**/*.php', '!**/*~']) // don't copy backup files
+        .pipe(changed('dist/blog/'))
+        .pipe(debug({title: 'gulp-debug [blog]'}))
+        .pipe(gulp.dest('dist/blog/'));
 });
