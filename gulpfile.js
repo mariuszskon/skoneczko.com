f335884778a5465c7dfc77a610a2d82b0c31529d
Mariusz Skoneczko <mariuszskon@users.noreply.github.com>
2015-09-12 18:40:33 +1000

    gulpfile can now execute a full build (copying everything it doesn't need to process), showing all of the files that it takes

--- gulpfile.js
+++ gulpfile.js
@@ -23,25 +23,39 @@ gulp.task('sass', function() {
 gulp.task('html', function() {
     gulp.src(['src/**/*.html', '!**/*~']) // don't copy backup files
         .pipe(changed('dist/'))
-        .pipe(debug({title: 'gulp-debug [html]'}))
+        .pipe(debug({title: 'gulp-debug [html]:'}))
         .pipe(minifyHTML({conditionals: true}))
         .pipe(gulp.dest('dist/'));
 });
 
 // blog task separate, in case changes are to be made later
 gulp.task('blog', function() {
-    gulp.src(['src/blog/**/*.php', '!**/*~']) // don't copy backup files
+    gulp.src('src/blog/**/*.php')
         .pipe(changed('dist/blog/'))
-        .pipe(debug({title: 'gulp-debug [blog]'}))
+        .pipe(debug({title: 'gulp-debug [blog]:'}))
         .pipe(gulp.dest('dist/blog/'));
 });
 
 gulp.task('images', function() {
     gulp.src('src/img/**/*')
         .pipe(changed('dist/img/'))
-        .pipe(debug({title: 'gulp-debug [images]'}))
+        .pipe(debug({title: 'gulp-debug [images]:'}))
         .pipe(imagemin())
         .pipe(gulp.dest('dist/img/'));
 });
 
-gulp.task('build', ['sass', 'html', 'blog', 'images']);
+// copy everything else that we may expect in the final build
+gulp.task('misc', function() {
+    gulp.src('src/favicon.ico')
+        .pipe(changed('dist/'))
+        .pipe(debug({title: 'gulp-debug [misc-favicon]:'}))
+        .pipe(gulp.dest('dist/'));
+    
+    //copy portfolio
+    gulp.src(['src/portfolio/**/*', '!**/*.html']) // don't get the html
+        .pipe(changed('dist/portfolio/'))
+        .pipe(debug({title: 'gulp-debug [misc-portfolio]:'}))
+        .pipe(gulp.dest('dist/portfolio/'));
+});
+
+gulp.task('build', ['sass', 'html', 'blog', 'images', 'misc']);
