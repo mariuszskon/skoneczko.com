ca27308bc3da37577c2a9dc0dc49bda543e9c7b5
Mariusz Skoneczko <mariuszskon@users.noreply.github.com>
2015-11-28 19:51:07 +1100

    Added information about crypto-toolkit

---
+++ src/blog/2015/11/28/check-out-crypto-toolkit.php
@@ -0,0 +1,5 @@
+<?php $pagetitle = "Check out crypto-toolkit!";
+$postdate = "28th November 2015";
+require "../../../includes/header.php"; ?>
+<p>Recently I was working on a JavaScript cryptography toolkit as a side-project. The result is <a href="https://github.com/mariuszskon/crypto-toolkit">crypto-toolkit</a>. Obviously, don't use it in serious operations because it is probably full of flaws that a hobbyist "cryptographer" like me wouldn't notice. But it is a fun tool nevertheless. The <a href="https://github.com/mariuszskon/crypto-toolkit">source code</a> is licensed under the MIT license and you can imediately use the tool online <a href="http://mariuszskon.github.io/crypto-toolkit">right now</a>.</p>
+<?php require "../../../includes/footer.php" ?>
